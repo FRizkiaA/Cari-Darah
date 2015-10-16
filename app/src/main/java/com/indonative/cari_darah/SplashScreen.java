2f7b56f5d061a5a39c55365ef59b2e2f95d5a9e5
FRizkiaA <fykarizkiaauliandy@yahoo.com>
2015-10-16 16:46:46 +0700

    Add Splash Screen

---
+++ app/src/main/java/com/indonative/cari_darah/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.indonative.cari_darah;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by Rizkia on 16/10/2015.
+ */
+public class SplashScreen extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread()
+        {
+          public void run()
+          {
+              try
+              {
+                sleep(3000);
+              }
+              catch (InterruptedException e)
+              {
+                  e.printStackTrace();
+              }
+              finally
+              {
+                  Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                  startActivity(intent);
+              }
+          }
+        };
+
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        finish();
+    }
+}
